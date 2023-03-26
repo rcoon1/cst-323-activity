80bac36e733e2671e53262b6d16f5b38fd846efb
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2023-03-25 21:05:27 -0500

    update

--- src/test/java/com/spring/SpringThymleafJpaApplicationTests.java
+++ src/test/java/com/spring/SpringThymleafJpaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kemal.spring;
+package com.spring;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
--- src/test/java/com/spring/domain/UserTest.java
+++ src/test/java/com/spring/domain/UserTest.java
@@ -1,10 +1,8 @@
-package com.kemal.spring.domain;
+package com.spring.domain;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Created by Keno&Kemo on 04.11.2017..
- */
+
 public class UserTest {
     @Test
     public void setRoles() throws Exception {
