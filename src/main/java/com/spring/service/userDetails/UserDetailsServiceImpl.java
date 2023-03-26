5b07f24d37df9e6b2fb646f17ee48c98a3a30848
rcoon1 <78291341+rcoon1@users.noreply.github.com>
2023-03-25 20:20:23 -0500

    init

---
+++ src/main/java/com/spring/service/userDetails/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.spring.service.userDetails;
+
+import com.spring.domain.User;
+import com.spring.service.UserService;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByEmail(username);
+        if (user == null) throw new UsernameNotFoundException(username);
+        return new UserDetailsImpl(user);
+    }
+}
